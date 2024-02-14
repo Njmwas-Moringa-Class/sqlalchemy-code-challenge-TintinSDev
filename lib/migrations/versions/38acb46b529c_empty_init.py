19c5d1c4e34c227093d9f9002a835b1427a02f8c
Martin <mmaina290@gmail.com>
2024-02-14 19:06:20 +0300

    done implemented seeds.py with faker

---
+++ lib/migrations/versions/38acb46b529c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: 38acb46b529c
+Revises: 
+Create Date: 2024-02-13 21:06:04.751453
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38acb46b529c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
